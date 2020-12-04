c2b403b7c8a9b18c98f177701243d26a31eeffea
Carlos <carloshalbuquerque2@gmail.com>
2020-12-04 09:02:12 -0300

    add first class DOM

---
+++ javascript-origamid/DOM/ex01 copy/script.js
@@ -0,0 +1,21 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = location.href;
+console.log(url)
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementActive = document.querySelector('.ativo');
+console.log(elementActive)
+
+const elementsActive = document.querySelectorAll('.ativo');
+console.log(elementsActive[1])
+
+// Retorne a linguagem do navegador
+const languageNavigator = navigator.language;
+console.log(languageNavigator)
+
+
+// Retorne a largura da janela 
+const Width = innerWidth;
+console.log(innerWidth)
+
