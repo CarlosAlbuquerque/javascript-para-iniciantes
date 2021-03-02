81e11619db1ae9912352bb175dcef30f892fc24e
Carlos <carloshalbuquerque2@gmail.com>
2021-03-02 08:51:51 -0300

    add Prime Numbers

---
+++ mini-projects/NumerosPrimos/script.js
@@ -0,0 +1,23 @@
+// Criar uma função para mostrar os números primos.
+
+function displayPrimeNumbers(limit){
+    for(let number = 2; number <= limit; number++){
+
+    const itsPrime = ItsPrime(number)
+
+        if (itsPrime)console.log(number)
+    }
+}
+
+function ItsPrime(number){
+    let prime = true
+
+    for (let div = 2; div < number; div++) {
+        if (number % div === 0) {
+            return prime = false
+        }
+        return prime = true
+    }
+}
+
+displayPrimeNumbers(15)
