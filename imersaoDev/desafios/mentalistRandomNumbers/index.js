d382c3b953278e76896144a965bad22d67804306
Carlos <carloshalbuquerque2@gmail.com>
2021-03-25 08:53:01 -0300

    version alternative mentalist

--- imersaoDev/desafios/mentalistRandomNumbers/index.js
+++ imersaoDev/desafios/mentalistRandomNumbers/index.js
@@ -11,14 +11,29 @@ function getRandomInt(min, max) {
         if (numberSecret === Number(kick.value)) {
             res.style.color = "white";
             res.innerHTML = `<p>Acertou! O número secreto era ${numberSecret}.</p>`
-        } else if(Number(kick.value) > numberSecret){
+            res.innerHTML += `<p>O jogo será reiniciado!</p>`
+            attPage()
+            } else if(Number(kick.value) > numberSecret){
             res.style.color = "white";
-            res.innerHTML = `<p>Número secreto é menor que o valor digitado.</p>`
+            res.innerHTML = `<p>Errou! O número secreto era menor =(.</p>`
+            res.innerHTML += `<p> O número secreto era ${numberSecret}.</p>`
+            res.innerHTML += `<p>O jogo será reiniciado!</p>`
+            attPage()
         } else if(Number(kick.value) < numberSecret){
             res.style.color = "white";
-            res.innerHTML = `<p>Número secreto é maior que o valor digitado.</p>`
+            res.innerHTML = `<p>Errou! O número secreto era maior =(.</p>`
+            res.innerHTML += `<p> O número secreto era ${numberSecret}.</p>`
+            res.innerHTML += `<p>O jogo será reiniciado!</p>`
+            attPage()
         }
     }
 
 
-// colocar pro usuario ter 3 tentativas
+    function attPage(){
+    let meta = document.createElement('meta');
+    meta.httpEquiv = "refresh";
+    meta.content = "5";
+    document.getElementsByTagName('head')[0].appendChild(meta);
+    }
+
+    // colocar pro usuario ter 3 tentativas
